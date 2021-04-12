181083cf6ec292e18a0393ec05a47539220660d4
Daniel <danielguve2@gmail.com>
2021-04-12 17:13:06 -0400

    imports de componentes material y sidenav inicial

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
-  declarations: [],
   exports: [
-
+    MatSidenavModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule
   ]
 })
 export class MaterialModule { }
